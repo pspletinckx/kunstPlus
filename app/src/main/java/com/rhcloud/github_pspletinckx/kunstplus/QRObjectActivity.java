35d0c89a9af1ad0a5c9b9a3be80fc6b7cfff88c1
Pieter Pletinckx <pieplay@gmail.com>
2015-03-29 18:54:40 +0200

    QRObjectActivity +Webrender -Intent - Like button QRScanActivity - external code intent - internal code intent - send to socket MainActivity -Advertizement -Localisation integration

---
+++ app/src/main/java/com/rhcloud/github_pspletinckx/kunstplus/QRObjectActivity.java
@@ -0,0 +1,52 @@
+package com.rhcloud.github_pspletinckx.kunstplus;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class QRObjectActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrobject);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        //WebView webView = (WebView)this.findViewById(R.id.qr_render);
+        webView.loadUrl("http://github-pspletinckx.rhcloud.com");
+        //webView.loadUrl("http://users.hogent.be/pspletinckx");
+        //webView.loadUrl("http://github-pspletinckx.rhcloud.com/kazouAalst");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_qrobject, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        switch (id){
+            case R.id.like:
+                //setResult url like to be added to local db
+                return true;
+            default:
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
