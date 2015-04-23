fab9b7e79d570d3c7d094a03c549f8c9ddb2dd01
Pieter Pletinckx <pieplay@gmail.com>
2015-04-23 17:07:16 +0200

    HomeScreenFragment moved to seperate file

--- app/src/main/java/com/rhcloud/github_pspletinckx/kunstplus/AppMainActivity.java
+++ app/src/main/java/com/rhcloud/github_pspletinckx/kunstplus/AppMainActivity.java
@@ -56,7 +56,7 @@ public class AppMainActivity extends ActionBarActivity
         
         //Pieter: Implement different Fragments here
         FragmentManager fragmentManager = getSupportFragmentManager();
-        if(position ==1){
+        if(position ==0){
 
             fragmentManager.beginTransaction()
                     .replace(R.id.container, HomeScreenFragment.newInstance(position + 1))
@@ -161,67 +161,4 @@ public class AppMainActivity extends ActionBarActivity
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
-
-    public static class HomeScreenFragment extends Fragment {
-        private String QRresource;
-        private ListView advList;
-
-        String[] web = {
-                "In My Head-Carll Cneut - ma-vr, 16u - 20u - Naam Museum - 1Km",
-                "Joris Ghekiere - Vandaag, 16u - 20u - Naam Museum - 200m ",
-                "In My Head-Carll Cneut - ma-vr, 16u - 20u - Naam Museum - 1Km"
-
-        } ;
-        Integer[] imageId = {
-                R.drawable.carllcneut480,
-                R.drawable.jorisghekiere480,
-                R.drawable.carllcneut480
-        };
-
-        public HomeScreenFragment() {
-        }
-
-        public static HomeScreenFragment newInstance(int sectionNumber) { //factory??
-            HomeScreenFragment fragment = new HomeScreenFragment();
-            Bundle args = new Bundle();
-
-            fragment.setArguments(args);
-            return fragment;
-        }
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_main, container, false);
-            AdvListAdaptor adaptor = new AdvListAdaptor(getActivity(),web,imageId);
-            advList = (ListView)rootView.findViewById(R.id.advList);
-            advList.setAdapter(adaptor);
-            return rootView;
-
-        }
-        /* Translate
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            AdvListAdaptor adaptor = new AdvListAdaptor(this,web,imageId);
-            advList = (ListView)findViewById(R.id.advList);
-            advList.setAdapter(adaptor);
-
-        }
-        */
-
-        private void voorbeeldqrview(){
-            //QRresource = "http://github-pspletinckx.rhcloud.com";
-            QRresource = "http://github-pspletinckx.rhcloud.com/kunstPlus/QR/123456789ABCDEF/";
-            Intent intent = new Intent(getActivity(),QRObjectActivity.class);
-            intent.putExtra("LoadResource",QRresource);
-            startActivity(intent);
-        }
-        private void toonqrscan(){
-            Intent intent = new Intent(getActivity(),QRScanActivity.class);
-            startActivity(intent);
-        }
-
-    }
-
 }
--- app/src/main/java/com/rhcloud/github_pspletinckx/kunstplus/HomeScreenFragment.java
+++ app/src/main/java/com/rhcloud/github_pspletinckx/kunstplus/HomeScreenFragment.java
@@ -1,109 +1,73 @@
 package com.rhcloud.github_pspletinckx.kunstplus;
 
-import android.app.Activity;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link HomeScreenFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link HomeScreenFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeScreenFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private String QRresource;
+    private ListView advList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    String[] web = {
+            "In My Head-Carll Cneut - ma-vr, 16u - 20u - Naam Museum - 1Km",
+            "Joris Ghekiere - Vandaag, 16u - 20u - Naam Museum - 200m ",
+            "In My Head-Carll Cneut - ma-vr, 16u - 20u - Naam Museum - 1Km"
 
-    private OnFragmentInteractionListener mListener;
+    } ;
+    Integer[] imageId = {
+            R.drawable.carllcneut480,
+            R.drawable.jorisghekiere480,
+            R.drawable.carllcneut480
+    };
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeScreenFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeScreenFragment newInstance(String param1, String param2) {
+    public HomeScreenFragment() {
+    }
+
+    public static HomeScreenFragment newInstance(int sectionNumber) { //factory??
         HomeScreenFragment fragment = new HomeScreenFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
-
-    public HomeScreenFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false);
-    }
+        View rootView = inflater.inflate(R.layout.activity_main, container, false);
+        AdvListAdaptor adaptor = new AdvListAdaptor(getActivity(),web,imageId);
+        advList = (ListView)rootView.findViewById(R.id.advList);
+        advList.setAdapter(adaptor);
+        return rootView;
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    }
+        /* Translate
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            AdvListAdaptor adaptor = new AdvListAdaptor(this,web,imageId);
+            advList = (ListView)findViewById(R.id.advList);
+            advList.setAdapter(adaptor);
+
         }
-    }
+        */
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    private void voorbeeldqrview(){
+        //QRresource = "http://github-pspletinckx.rhcloud.com";
+        QRresource = "http://github-pspletinckx.rhcloud.com/kunstPlus/QR/123456789ABCDEF/";
+        Intent intent = new Intent(getActivity(),QRObjectActivity.class);
+        intent.putExtra("LoadResource",QRresource);
+        startActivity(intent);
     }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    private void toonqrscan(){
+        Intent intent = new Intent(getActivity(),QRScanActivity.class);
+        startActivity(intent);
     }
 
 }
+
+
